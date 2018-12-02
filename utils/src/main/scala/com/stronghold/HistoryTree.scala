9e300bdc5b8d8f472ccfc87cd2b49d5cd3177154
root <root@linux-zrx9.fios-router.home>
2018-12-02 11:37:12 -0500

    Just the barebones of HistoryTree and assorted tests. Added a stand in Time construct

--- utils/src/main/scala/com/stronghold/HistoryTree.scala
+++ utils/src/main/scala/com/stronghold/HistoryTree.scala
@@ -1,5 +1,25 @@
 package com.stronghold
 
-class HistoryTree {
+
+trait HistoryTree {
+  def addValue(elem: HistoryTree): HistoryTree
+  def valueAtTime(t: Time): Option[Any]
+}
+
+class EmptyHTree extends HistoryTree {
+  val time: Option[Time] = new Nothing
+  val value: Option[Any] = new Nothing
+  def addValue(elem: HistoryTree): HistoryTree = elem
+  def valueAtTime(t: Time): Option[Any] = new Nothing
+}
+
+class HTree(time: Option[Time], value: Option[Any],
+            left: HistoryTree = new EmptyHTree, right: HistoryTree = new EmptyHTree) extends HistoryTree with Ordered[HTree] {
+  def compare(thatTree: HTree): Int = ???
+  def addValue(elem: HistoryTree): HTree = ???
+  def valueAtTime(t: Time): Option[Any] = ???
+
 
 }
+
+
